d51af1b8aaa254467f50b915a5b14681a303f157
Okechukwu Egeruoh <39450490+Chukwu12@users.noreply.github.com>
2025-02-23 17:38:48 +0000

    update

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,3 +1,4 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import React from 'react';
 import { ChakraProvider } from '@chakra-ui/react'; 
 import Navbar from './components/Navbar/Navbar';
@@ -13,9 +14,22 @@ import Footer from './components/Footer/Footer';
 const App = () => {
   return (
     <ChakraProvider> 
+      <BrowserRouter>
+      <Navbar />
+      <Routes>
+          {/* Define routes */}
+          <Route path="/" element={<Home />} /> {/* Displays Home page when user visits "/" */}
+          <Route path="/shop" element={<Shop />} /> {/* Displays Shop page */}
+          <Route path="/CheeseCakes" element={<CheeseCakesPage />} />
+        <Route path="/Cupcakes" element={<CupcakesPage />} />
+        <Route path="/DessertBoxs" element={<DessertBoxsPage />} />
+        <Route path="/Puddings" element={<PuddingsPage />} />
+        <Route path="/Puddings-Flavors" element={<PuddingsFlavorsPage />} />
+        <Route path="/Mini-Puddings-Cups" element={<MiniPuddingsCupsPage />} />
+        <Route path="/Cookies" element={<CookiesPage />} />
+      </Routes>
       <main className='overflow-x-hidden'>
          {/* Main Layout Components */}
-        <Navbar />
         <Hero />
         <Menus />
         <Banner />
@@ -24,6 +38,7 @@ const App = () => {
         <TestimonialsSection />
         <Footer /> 
       </main>
+      </BrowserRouter>
     </ChakraProvider>
     
   );
--- my-project/src/components/Navbar/Shoplinks.jsx
+++ my-project/src/components/Navbar/Shoplinks.jsx
@@ -7,44 +7,43 @@ function ShopLink() {
 
     
   return (
-    <Menu  >
-      <MenuButton className="hover:text-[#5EC49D] transition-all duration-150 ease"  >
-        <ChakraLink as={Link} className="flex items-center gap-2" to="/shop">
-          SHOP <FaAngleDown />
-        </ChakraLink>
-      </MenuButton>
+    <Menu>
+  <MenuButton className="hover:text-[#5EC49D] transition-all duration-150 ease">
+    <ChakraLink as={Link} className="flex items-center gap-2" to="/shop">
+      SHOP <FaAngleDown />
+    </ChakraLink>
+  </MenuButton>
 
-      <MenuList display={'flex'} flexDirection={'column'}  gap={'10px'} >
-        <MenuItem className="hover:text-[#5EC49D] transition-all duration-150 ease" as={Link} to="/organic-fruits">
-        CheeseCakes
-        </MenuItem>
+  <MenuList display={'flex'} flexDirection={'column'} gap={'10px'}>
+    <MenuItem className="hover:text-[#5EC49D] transition-all duration-150 ease" as={Link} to="/CheeseCakes">
+      CheeseCakes
+    </MenuItem>
 
-         <MenuItem className="hover:text-[#5EC49D] transition-all duration-150 ease" as={Link} to="/aggregate-fruits">
-         Cupcakes
-        </MenuItem>
+    <MenuItem className="hover:text-[#5EC49D] transition-all duration-150 ease" as={Link} to="/Cupcakes">
+      Cupcakes
+    </MenuItem>
 
-           <MenuItem className="hover:text-[#5EC49D] transition-all duration-150 ease" as={Link} to="/pits-fruits">
-          DessertBoxs
-        </MenuItem>
-        
-        <MenuItem className="hover:text-[#5EC49D] transition-all duration-150 ease" as={Link} to="/legumas-fruits">
-         Puddings
-        </MenuItem>
+    <MenuItem className="hover:text-[#5EC49D] transition-all duration-150 ease" as={Link} to="/DessertBoxs">
+      DessertBoxs
+    </MenuItem>
 
-        <MenuItem className="hover:text-[#5EC49D] transition-all duration-150 ease" as={Link} to="/legumas-fruits">
-         Puddings Flavors
-        </MenuItem>
+    <MenuItem className="hover:text-[#5EC49D] transition-all duration-150 ease" as={Link} to="/Puddings">
+      Puddings
+    </MenuItem>
 
-        <MenuItem className="hover:text-[#5EC49D] transition-all duration-150 ease" as={Link} to="/legumas-fruits">
-         Mini Puddings Cups
-        </MenuItem>
+    <MenuItem className="hover:text-[#5EC49D] transition-all duration-150 ease" as={Link} to="/Puddings-Flavors">
+      Puddings Flavors
+    </MenuItem>
 
-        <MenuItem className="hover:text-[#5EC49D] transition-all duration-150 ease" as={Link} to="/legumas-fruits">
-         Cookies
-        </MenuItem>
+    <MenuItem className="hover:text-[#5EC49D] transition-all duration-150 ease" as={Link} to="/Mini-Puddings-Cups">
+      Mini Puddings Cups
+    </MenuItem>
 
-      </MenuList>
-    </Menu>
+    <MenuItem className="hover:text-[#5EC49D] transition-all duration-150 ease" as={Link} to="/Cookies">
+      Cookies
+    </MenuItem>
+  </MenuList>
+</Menu>
   );
 }
 
