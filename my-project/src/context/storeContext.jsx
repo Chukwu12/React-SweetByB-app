6aba3a22df3c0bf95682c04a05827da89bca4a3d
Okechukwu Egeruoh <39450490+Chukwu12@users.noreply.github.com>
2025-11-04 15:40:52 +0000

    Update fetch URL to Railway backend and environment variable

--- my-project/src/context/storeContext.jsx
+++ my-project/src/context/storeContext.jsx
@@ -12,8 +12,7 @@ const StoreContextProvider = (props) => {
     useEffect(() => {
         const fetchProducts = async () => {
             try {
-                const response = await axios.get('https://possessed-cape-6rx4xv74vr4hxvqj-5000.app.github.dev/api/foods/list');
-
+                const response = await axios.get('https://discerning-strength-production.up.railway.app/api/foods/list');
                 if (response.data.success) {
                     setProducts(response.data.data); // Set the fetched data to state
                 } else {
