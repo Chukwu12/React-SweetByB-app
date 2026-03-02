007764079dc2e51bb7a129dd35ea6b3bee73bfac
Okechukwu Egeruoh <39450490+Chukwu12@users.noreply.github.com>
2026-03-02 13:42:56 +0000

    updated netlify url to deploy

--- backend/server.js
+++ backend/server.js
@@ -154,7 +154,6 @@ app.use((err, req, res, next) => {
 
 
 // Server Running
-const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`✅ Server running on port ${PORT} (${process.env.NODE_ENV || 'development'} mode)`);
 });
